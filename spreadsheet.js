746fea259c9c00d7f0449f16c4ff9a47b2cd6715
AnhNT <tienanhnguyen191999@gmail.com>
2022-06-20 21:16:48 +0700

    init project

---
+++ spreadsheet.js
@@ -0,0 +1,45 @@
+const sheetId = '13iCzj9GlPs963RfEvmutPca2if99T0ARQ355kxJyQcQ';
+const base = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;
+const apiKey = "AIzaSyBP8s-zVM5K7qLT5hbmsxy0xpQWtURwmD0";
+const tab_name = "Sheet1";
+
+const url = 'https://sheets.googleapis.com/v4/spreadsheets/' +
+           sheetId + '/values/' + tab_name +
+           '?alt=json&key=' + apiKey;
+
+const data = []
+
+document.addEventListener('DOMContentLoaded', init)
+function init() {
+    fetch(url)
+        .then(res => res.text())
+        .then(rep => {
+            //Remove additional text and extract only JSON:
+            const jsonData = JSON.parse(rep.substring(47).slice(0, -2));
+            console.log(jsonData)
+ 
+            const colz = [];
+            const tr = document.createElement('tr');
+            //Extract column labels
+            jsonData.table.cols.forEach((heading) => {
+                if (heading.label) {
+                    let column = heading.label;
+                    colz.push(column);
+                    const th = document.createElement('th');
+                    th.innerText = column;
+                    tr.appendChild(th);
+                }
+            })
+            output.appendChild(tr);
+ 
+            //extract row data:
+            jsonData.table.rows.forEach((rowData) => {
+                const row = {};
+                colz.forEach((ele, ind) => {
+                    row[ele] = (rowData.c[ind] != null) ? rowData.c[ind].v : '';
+                })
+                data.push(row);
+            })
+            processRows(data);
+        })
+}
