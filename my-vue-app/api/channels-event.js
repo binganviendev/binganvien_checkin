ab45b2413d9307d03a50402d15e25aae8efb89e8
AnhNT <tienanhnguyen191999@gmail.com>
2022-06-26 14:47:59 +0700

    test serverless

--- my-vue-app/api/channels-event.js
+++ my-vue-app/api/channels-event.js
@@ -1,4 +1,9 @@
-var Pusher = require("pusher");
+const Pusher = require('pusher');
+
+const appId = "1428573";
+const key = "ded5041fb98b0734a8cf";
+const secret = "98faa35b224a20255be3";
+const cluster = "ap1";
 
 var pusher = new Pusher({
   appId,
