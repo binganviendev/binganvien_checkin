f1fa9f7612207f8388ca22a979c30f58031a114a
AnhNT <tienanhnguyen191999@gmail.com>
2022-06-26 14:44:48 +0700

    test serverless

--- my-vue-app/api/channels-event.js
+++ my-vue-app/api/channels-event.js
@@ -1,11 +1,6 @@
-const Pusher = require('pusher');
+var Pusher = require("pusher");
 
-const appId = "1428573";
-const key = "ded5041fb98b0734a8cf";
-const secret = "98faa35b224a20255be3";
-const cluster = "ap1";
-
-const pusher = new Pusher({
+var pusher = new Pusher({
   appId,
   key,
   secret,
@@ -13,11 +8,7 @@ const pusher = new Pusher({
 });
 
 module.exports = (req, res) => {
-  const data = req.body;
-
   const channel = 'binganvien';
   const event = 'broadcast';
-  pusher.trigger(channel, event, data, () => {
-    res.status(200).end('sent event successfully');
-  });
-};
+  pusher.trigger(channel, event, { message: "hello world" });
+}
