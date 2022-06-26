e58902a86ed35e631b567f6e387fcb0fe5778590
AnhNT <tienanhnguyen191999@gmail.com>
2022-06-26 17:32:49 +0700

    debug

--- my-vue-app/api/pusher-auth.js
+++ my-vue-app/api/pusher-auth.js
@@ -1,5 +1,5 @@
 
 module.exports = (req, res) => {
   // res.status(200).end({ "auth": "random_token", "user_info": "my_user_info" });
-  res.json({ "auth": "random_token", "user_info": "my_user_info" })
+  res.json({ auth: "$2a$14$dKlTzyT/GtxOO9m.fIDOEeuoGDgZ29y9FNjeX9VCT329UpKmqQlEO", user_info: "my_user_info" })
 }
