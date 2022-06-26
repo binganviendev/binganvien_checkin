163c39a8a6d944ff3faeabd937bdab051971f3f8
AnhNT <tienanhnguyen191999@gmail.com>
2022-06-26 17:48:49 +0700

    debug

--- my-vue-app/api/pusher-auth.js
+++ my-vue-app/api/pusher-auth.js
@@ -16,7 +16,10 @@ var pusher = new Pusher({
 module.exports = (req, res) => {
   const socketId = req.body.socket_id;
   // res.json({ auth: "$2a$14$dKlTzyT/GtxOO9m.fIDOEeuoGDgZ29y9FNjeX9VCT329UpKmqQlEO", user_info: "my_user_info" })
-  const user = {id: 12345}; // Replace this with code to retrieve the actual user id
+  const user = {
+    id: + new Date(),
+    name: + new Date()
+  }; // Replace this with code to retrieve the actual user id
   const authResponse = pusher.authenticateUser(socketId, user);
   res.send(authResponse);
 }
